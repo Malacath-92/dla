9e58e5c6a3f9dde4ca2fbd84ec037416d3e28180
Hanno <h.baensch.92@gmail.com>
2018-12-22 16:51:52 +0100

    Replaced decay_t in ´tests with a more appropriate remove_const_t

--- test/main.cpp
+++ test/main.cpp
@@ -32,41 +32,41 @@ int main() {
     constexpr auto length = 1.0_m;
     constexpr auto time = 1.0_s;
     constexpr auto weight = 1.0_kg;
-	static_assert(std::is_same_v<std::decay_t<decltype(length)>, length_unit>);
-	static_assert(std::is_same_v<std::decay_t<decltype(time)>, time_unit>);
-	static_assert(std::is_same_v<std::decay_t<decltype(weight)>, weight_unit>);
+	static_assert(std::is_same_v<std::remove_const_t<decltype(length)>, length_unit>);
+	static_assert(std::is_same_v<std::remove_const_t<decltype(time)>, time_unit>);
+	static_assert(std::is_same_v<std::remove_const_t<decltype(weight)>, weight_unit>);
 
 	constexpr auto frequency = 1.0f / time;
-	static_assert(std::is_same_v<std::decay_t<decltype(frequency)>, frequency_unit>);
+	static_assert(std::is_same_v<std::remove_const_t<decltype(frequency)>, frequency_unit>);
 
     constexpr auto volume = length * length * length;
-	static_assert(std::is_same_v<std::decay_t<decltype(volume)>, volume_unit>);
+	static_assert(std::is_same_v<std::remove_const_t<decltype(volume)>, volume_unit>);
 
 	constexpr auto velocity = length / time;
 	constexpr auto same_velocity = frequency * length;
-	static_assert(std::is_same_v<std::decay_t<decltype(velocity)>, velocity_unit>);
-	static_assert(std::is_same_v<std::decay_t<decltype(velocity)>, std::decay_t<decltype(same_velocity)>>);
+	static_assert(std::is_same_v<std::remove_const_t<decltype(velocity)>, velocity_unit>);
+	static_assert(std::is_same_v<std::remove_const_t<decltype(velocity)>, std::remove_const_t<decltype(same_velocity)>>);
 
 	constexpr auto acceleration = length / time / time;
 	constexpr auto same_acceleration = frequency * frequency * length;
-	static_assert(std::is_same_v<std::decay_t<decltype(acceleration)>, acceleration_unit>);
-	static_assert(std::is_same_v<std::decay_t<decltype(acceleration)>, std::decay_t<decltype(same_acceleration)>>);
+	static_assert(std::is_same_v<std::remove_const_t<decltype(acceleration)>, acceleration_unit>);
+	static_assert(std::is_same_v<std::remove_const_t<decltype(acceleration)>, std::remove_const_t<decltype(same_acceleration)>>);
 
 	constexpr auto force = acceleration * weight;
 	constexpr auto same_force = (frequency * length) * (weight / time);
-	static_assert(std::is_same_v<std::decay_t<decltype(force)>, force_unit>);
-	static_assert(std::is_same_v<std::decay_t<decltype(force)>, std::decay_t<decltype(same_force)>>);
+	static_assert(std::is_same_v<std::remove_const_t<decltype(force)>, force_unit>);
+	static_assert(std::is_same_v<std::remove_const_t<decltype(force)>, std::remove_const_t<decltype(same_force)>>);
 
 	constexpr auto dim_less = length / length;
 	constexpr auto other_dim_less = velocity / same_velocity;
 	constexpr auto more_dim_less = time * acceleration / length / frequency;
-	static_assert(std::is_same_v<std::decay_t<decltype(dim_less)>, float>);
-	static_assert(std::is_same_v<std::decay_t<decltype(other_dim_less)>, float>);
-	static_assert(std::is_same_v<std::decay_t<decltype(more_dim_less)>, float>);
+	static_assert(std::is_same_v<std::remove_const_t<decltype(dim_less)>, float>);
+	static_assert(std::is_same_v<std::remove_const_t<decltype(other_dim_less)>, float>);
+	static_assert(std::is_same_v<std::remove_const_t<decltype(more_dim_less)>, float>);
 
 	auto length_sqrt = math::sqrt(length);
 	auto length_cbrt = math::cbrt(length);
 	auto length_full = math::pow<2>(length_sqrt);
 	length_full = math::pow<3>(length_cbrt);
-	static_assert(std::is_same_v<std::decay_t<decltype(length_full)>, length_unit>);
+	static_assert(std::is_same_v<std::remove_const_t<decltype(length_full)>, length_unit>);
 }
