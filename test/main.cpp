60ec81ad63ce67d39a88fd816c8ea59fc20e2a06
Hanno <h.baensch.92@gmail.com>
2018-12-29 12:16:22 +0100

    Testing implicit conversion

--- test/main.cpp
+++ test/main.cpp
@@ -5,7 +5,7 @@
 
 #include <iostream>
 
-#include "tuple_sort.h"
+#include "detail/tuple_sort.h"
 
 template <class T, class U>
 struct descending {
@@ -103,4 +103,16 @@ int main() {
 	using vector::vec1;
 	constexpr auto only_one_element = vec1(1.0f);
 	[[maybe_unused]] constexpr float just_that_element = only_one_element;
+
+	using vector::uvec3;
+	constexpr auto float_vec = vec3(1.0f);
+	constexpr auto double_vec = dvec3(1.0f);
+	constexpr auto int_vec = ivec3(1);
+	constexpr auto uint_vec = uvec3(1u);
+	constexpr auto fd_prod_vec = float_vec * double_vec;
+	constexpr auto id_prod_vec = int_vec * double_vec;
+	constexpr auto iu_prod_vec = int_vec * uint_vec;
+	static_assert(std::is_same_v<std::remove_const_t<decltype(fd_prod_vec)>, dvec3>);
+	static_assert(std::is_same_v<std::remove_const_t<decltype(id_prod_vec)>, dvec3>);
+	static_assert(std::is_same_v<std::remove_const_t<decltype(iu_prod_vec)>, uvec3>);
 }
