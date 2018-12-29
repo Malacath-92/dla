c5e0985a0631065305e2cadd81cfa78a8073709b
Hanno <h.baensch.92@gmail.com>
2018-12-29 11:43:03 +0100

    Split scalar math into .h and .inl

--- include/scalar_math.h
+++ include/scalar_math.h
@@ -1,26 +1,13 @@
 #pragma once
 
-#include <cmath>
-
-#include "units.h"
-
-#include "detail/detail.h"
-
 // Note: Without constexpr math functions we can not make constexpr functions here either
 namespace unit::math {
 	template<std::intmax_t pNum, std::intmax_t pDen = 1, class T>
-    auto pow(const T& val) {
-		using result_type = detail::power_t<T, pNum, pDen>;
-		return result_type{ std::pow(float(val), float(pNum) / float(pDen)) };
-    }
+    auto pow(const T& val);
 	template<class T>
-    auto sqrt(const T& val) {
-		using result_type = detail::power_t<T, 1, 2>;
-        return result_type{ std::sqrt(float(val)) };
-    }
+    auto sqrt(const T& val);
 	template<class T>
-    auto cbrt(const T& val) {
-		using result_type = detail::power_t<T, 1, 3>;
-        return result_type{ std::cbrt(float(val)) };
-    }
+    auto cbrt(const T& val);
 }
+
+#include "scalar_math.inl"
---
+++ include/scalar_math.inl
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "scalar_math.h"
+
+#include <cmath>
+
+#include "units.h"
+
+#include "detail/detail.h"
+
+namespace unit::math {
+	template<std::intmax_t pNum, std::intmax_t pDen, class T>
+    auto pow(const T& val) {
+		using result_type = detail::power_t<T, pNum, pDen>;
+		return result_type{ std::pow(float(val), float(pNum) / float(pDen)) };
+    }
+	template<class T>
+    auto sqrt(const T& val) {
+		using result_type = detail::power_t<T, 1, 2>;
+        return result_type{ std::sqrt(float(val)) };
+    }
+	template<class T>
+    auto cbrt(const T& val) {
+		using result_type = detail::power_t<T, 1, 3>;
+        return result_type{ std::cbrt(float(val)) };
+    }
+}
