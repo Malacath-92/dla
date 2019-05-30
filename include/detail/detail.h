8bdc709d0b5ef8d290e05625c56ac542760d01a6
Hanno <h.baensch.92@gmail.com>
2019-05-30 10:30:43 +0200

    Fix ambiguous call

--- include/detail/detail.h
+++ include/detail/detail.h
@@ -97,6 +97,6 @@ namespace dla::detail {
 	}
 	template<class InputIt, class T>
 	constexpr T accumulate(InputIt first, const InputIt last, T&& init) {
-		return accumulate(first, last, std::move(init), std::plus<>());
+		return dla::detail::accumulate(first, last, std::move(init), std::plus<>());
 	}
 }
