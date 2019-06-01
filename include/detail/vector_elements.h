3a708b3a10afdc8f1bf19072449bb24fb5f43fbd
Hanno <h.baensch.92@gmail.com>
2019-06-01 17:18:25 +0200

    Add tests for converting constructors of mat

--- include/detail/vector_elements.h
+++ include/detail/vector_elements.h
@@ -11,6 +11,9 @@
 //          - uninitialized memory at runtime for better performance
 
 namespace dla::detail {
+	template<class T, class U>
+	inline constexpr auto is_element_convertible_v = std::is_convertible_v<T, U> || std::is_constructible_v<T, U>;
+
     template<class T, std::size_t N>
     struct vec_elements {
         static_assert(N > 0 && N <= 5, "Vectors only supported for sizes 1, 2, 3 and 4!");
@@ -30,82 +33,82 @@ namespace dla::detail {
 		template<class U = T, typename = std::enable_if_t<!std::is_same_v<const U&, T&&>>>
 		constexpr explicit vec_elements(T&& px) noexcept(std::is_nothrow_move_constructible_v<T>);
 
-        template<class U, typename = std::enable_if_t<std::is_convertible_v<U, T>>>
-        constexpr explicit vec_elements(const vec_elements<U, 1>& val);
-        template<class U, typename = std::enable_if_t<std::is_assignable_v<std::add_lvalue_reference_t<T>, U>>>
-        constexpr vec_elements& operator=(const vec_elements<U, 1>& val);
+        template<class U, typename = std::enable_if_t<is_element_convertible_v<U, T>>>
+		constexpr explicit vec_elements(const vec_elements<U, 1>& val);
+		template<class U, typename = std::enable_if_t<std::is_assignable_v<std::add_lvalue_reference_t<T>, U>>>
+		constexpr vec_elements& operator=(const vec_elements<U, 1>& val);
 
-        union { value_type x, r, s; };
-    };
-    template<class T>
-    struct vec_elements<T, 2> {
-        using value_type = wrapped_reference_t<T>;
+		union { value_type x, r, s; };
+	};
+	template<class T>
+	struct vec_elements<T, 2> {
+		using value_type = wrapped_reference_t<T>;
 
-        vec_elements() = default;
-        constexpr vec_elements(const vec_elements&) = default;
-        constexpr vec_elements(vec_elements&&) = default;
-        constexpr vec_elements& operator=(const vec_elements&) = default;
-        constexpr vec_elements& operator=(vec_elements&&) = default;
-        
-        constexpr explicit vec_elements(const T& val) noexcept(std::is_nothrow_copy_constructible_v<T>);
-        
-        constexpr vec_elements(const T& px, const T& py) noexcept(std::is_nothrow_copy_constructible_v<T>);
+		vec_elements() = default;
+		constexpr vec_elements(const vec_elements&) = default;
+		constexpr vec_elements(vec_elements&&) = default;
+		constexpr vec_elements& operator=(const vec_elements&) = default;
+		constexpr vec_elements& operator=(vec_elements&&) = default;
+
+		constexpr explicit vec_elements(const T& val) noexcept(std::is_nothrow_copy_constructible_v<T>);
+
+		constexpr vec_elements(const T& px, const T& py) noexcept(std::is_nothrow_copy_constructible_v<T>);
 
 		template<class U = T, typename = std::enable_if_t<!std::is_same_v<const U&, T&&>>>
 		constexpr vec_elements(T&& px, T&& py) noexcept(std::is_nothrow_move_constructible_v<T>);
 
-        template<class U, typename = std::enable_if_t<std::is_convertible_v<U, T>>>
-        constexpr explicit vec_elements(const vec_elements<U, 2>& val);
-        template<class U, typename = std::enable_if_t<std::is_assignable_v<std::add_lvalue_reference_t<T>, U>>>
-        constexpr vec_elements& operator=(const vec_elements<U, 2>& val);
-        
-        union { value_type x, r, s; };
-        union { value_type y, g, t; };
-    };
-    template<class T>
-    struct vec_elements<T, 3> {
-        using value_type = wrapped_reference_t<T>;
+		template<class U, typename = std::enable_if_t<is_element_convertible_v<U, T>>>
+		constexpr explicit vec_elements(const vec_elements<U, 2>& val);
+		template<class U, typename = std::enable_if_t<std::is_assignable_v<std::add_lvalue_reference_t<T>, U>>>
+		constexpr vec_elements& operator=(const vec_elements<U, 2>& val);
 
-        vec_elements() = default;
-        constexpr vec_elements(const vec_elements&) = default;
-        constexpr vec_elements(vec_elements&&) = default;
-        constexpr vec_elements& operator=(const vec_elements&) = default;
-        constexpr vec_elements& operator=(vec_elements&&) = default;
+		union { value_type x, r, s; };
+		union { value_type y, g, t; };
+	};
+	template<class T>
+	struct vec_elements<T, 3> {
+		using value_type = wrapped_reference_t<T>;
 
-        constexpr explicit vec_elements(const T& val) noexcept(std::is_nothrow_copy_constructible_v<T>);
-        
-        constexpr vec_elements(const T& px, const T& py, const T& pz) noexcept(std::is_nothrow_copy_constructible_v<T>);
+		vec_elements() = default;
+		constexpr vec_elements(const vec_elements&) = default;
+		constexpr vec_elements(vec_elements&&) = default;
+		constexpr vec_elements& operator=(const vec_elements&) = default;
+		constexpr vec_elements& operator=(vec_elements&&) = default;
+
+		constexpr explicit vec_elements(const T& val) noexcept(std::is_nothrow_copy_constructible_v<T>);
+
+		constexpr vec_elements(const T& px, const T& py, const T& pz) noexcept(std::is_nothrow_copy_constructible_v<T>);
 
 		template<class U = T, typename = std::enable_if_t<!std::is_same_v<const U&, T&&>>>
 		constexpr vec_elements(T&& px, T&& py, T&& pz) noexcept(std::is_nothrow_move_constructible_v<T>);
 
-        template<class U, typename = std::enable_if_t<std::is_convertible_v<U, T>>>
-        constexpr explicit vec_elements(const vec_elements<U, 3>& val);
-        template<class U, typename = std::enable_if_t<std::is_assignable_v<std::add_lvalue_reference_t<T>, U>>>
-        constexpr vec_elements& operator=(const vec_elements<U, 3>& val);
-        
-        union { value_type x, r, s; };
-        union { value_type y, g, t; };
-        union { value_type z, b, p; };
-    };
-    template<class T>
-    struct vec_elements<T, 4> {
-        using value_type = wrapped_reference_t<T>;
+		template<class U, typename = std::enable_if_t<is_element_convertible_v<U, T>>>
+		constexpr explicit vec_elements(const vec_elements<U, 3>& val);
+		template<class U, typename = std::enable_if_t<std::is_assignable_v<std::add_lvalue_reference_t<T>, U>>>
+		constexpr vec_elements& operator=(const vec_elements<U, 3>& val);
 
-        vec_elements() = default;
-        constexpr vec_elements(const vec_elements&) = default;
-        constexpr vec_elements(vec_elements&&) = default;
-        constexpr vec_elements& operator=(const vec_elements&) = default;
-        constexpr vec_elements& operator=(vec_elements&&) = default;
+		union { value_type x, r, s; };
+		union { value_type y, g, t; };
+		union { value_type z, b, p; };
+	};
+	template<class T>
+	struct vec_elements<T, 4> {
+		using value_type = wrapped_reference_t<T>;
 
-        constexpr explicit vec_elements(const T& val) noexcept(std::is_nothrow_copy_constructible_v<T>);
-        
-        constexpr vec_elements(const T& px, const T& py, const T& pz, const T& pw) noexcept(std::is_nothrow_copy_constructible_v<T>);
+		vec_elements() = default;
+		constexpr vec_elements(const vec_elements&) = default;
+		constexpr vec_elements(vec_elements&&) = default;
+		constexpr vec_elements& operator=(const vec_elements&) = default;
+		constexpr vec_elements& operator=(vec_elements&&) = default;
+
+		constexpr explicit vec_elements(const T& val) noexcept(std::is_nothrow_copy_constructible_v<T>);
+
+		constexpr vec_elements(const T& px, const T& py, const T& pz, const T& pw) noexcept(std::is_nothrow_copy_constructible_v<T>);
 
 		template<class U = T, typename = std::enable_if_t<!std::is_same_v<const U&, T&&>>>
-        constexpr vec_elements(T&& px, T&& py, T&& pz, T&& pw) noexcept(std::is_nothrow_move_constructible_v<T>);
+		constexpr vec_elements(T&& px, T&& py, T&& pz, T&& pw) noexcept(std::is_nothrow_move_constructible_v<T>);
 
-        template<class U, typename = std::enable_if_t<std::is_convertible_v<U, T>>>
+		template<class U, typename = std::enable_if_t<is_element_convertible_v<U, T>>>
         constexpr explicit vec_elements(const vec_elements<U, 4>& val);
         template<class U, typename = std::enable_if_t<std::is_assignable_v<std::add_lvalue_reference_t<T>, U>>>
         constexpr vec_elements& operator=(const vec_elements<U, 4>& val);
--- test/matrix_construct.cpp
+++ test/matrix_construct.cpp
@@ -15,6 +15,10 @@ using imat2x3 = tmat2x3<int>;
 using imat3x4 = tmat3x4<int>;
 using imat4x2 = tmat4x2<int>;
 
+using umat2 = tmat2<unsigned int>;
+using umat3 = tmat3<unsigned int>;
+using umat4 = tmat4<unsigned int>;
+
 constexpr auto matrix2x2_diag_expected = imat2({ 1, 0 }, { 0, 1 });
 constexpr auto matrix2x2_fill_expected = imat2({ 1, 1 }, { 1, 1 });
 constexpr auto matrix3x3_diag_expected = imat3({ 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 });
@@ -35,6 +39,20 @@ constexpr auto matrix2x3 = imat2x3(1);
 constexpr auto matrix3x4 = imat3x4(1);
 constexpr auto matrix4x2 = imat4x2(1);
 
+constexpr auto matrix2x2_conv_expected = umat2({ 1u, 0u }, { 0u, 1u });
+constexpr auto matrix3x3_conv_expected = umat3({ 1u, 0u, 0u }, { 0u, 1u, 0u }, { 0u, 0u, 1u });
+constexpr auto matrix4x4_conv_expected = umat4({ 1u, 0u, 0u, 0u }, { 0u, 1u, 0u, 0u }, { 0u, 0u, 1u, 0u }, { 0u, 0u, 0u, 1u });
+
+#ifdef _MSC_VER
+/*constexpr*/ auto matrix2x2_conv = umat2(matrix2x2_diag);
+/*constexpr*/ auto matrix3x3_conv = umat3(matrix3x3_diag);
+/*constexpr*/ auto matrix4x4_conv = umat4(matrix4x4_diag);
+#else
+constexpr auto matrix2x2_conv = umat2(matrix2x2_diag);
+constexpr auto matrix3x3_conv = umat3(matrix3x3_diag);
+constexpr auto matrix4x4_conv = umat4(matrix4x4_diag);
+#endif
+
 TEST_CASE("Generate matrices from single arguments", "[matrix_construct_single]") {
     REQUIRE(matrix2x2_diag == matrix2x2_diag_expected);
     REQUIRE(matrix2x2_fill == matrix2x2_fill_expected);
@@ -45,4 +63,10 @@ TEST_CASE("Generate matrices from single arguments", "[matrix_construct_single]"
     REQUIRE(matrix2x3 == matrix2x3_expected);
     REQUIRE(matrix3x4 == matrix3x4_expected);
     REQUIRE(matrix4x2 == matrix4x2_expected);
+}
+
+TEST_CASE("Copy construct matrices from other underlying type", "[matrix_construct_convert]") {
+    REQUIRE(matrix2x2_conv == matrix2x2_conv_expected);
+    REQUIRE(matrix3x3_conv == matrix3x3_conv_expected);
+    REQUIRE(matrix4x4_conv == matrix4x4_conv_expected);
 }
